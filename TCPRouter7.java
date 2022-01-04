7420b066fcf18dbcb77dc1696c9b2f66b52f3a48
Gayenur9 <97120027+Gayenur9@users.noreply.github.com>
2022-01-04 23:48:17 +0300

    Update TCPRouter7.java

--- TCPRouter7.java
+++ TCPRouter7.java
@@ -16,7 +16,7 @@ public class TCPRouter7
       {  
           try  
           {  
-        	  System.out.println("TCPRoute7 Başlatılıyor..\\nLocalhost'un ip Adresi Alınıyo..");
+        	  System.out.println("TCPRoute7 Başlatılıyor..\\n Localhost'un ip Adresi Alınıyo..");
         	  host = InetAddress.getLocalHost();  
           }  
           catch(Exception uhEx)  
